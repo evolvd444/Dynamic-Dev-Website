4fa85b2ddf66331ae83fdd4bd75a9abe060251a5
evolvd444 <86843762+evolvd444@users.noreply.github.com>
2023-02-16 13:00:34 -0500

    progress on upgrading

--- pages/api/getSkills.tsx
+++ pages/api/getSkills.tsx
@@ -1,6 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import {groq} from 'next-sanity'
-import {sanityClient} from "../../lib/sanity.client"
+import sanityClient from "../../lib/sanity.client"
 import { Skill } from '../../typings.d'
 
 
