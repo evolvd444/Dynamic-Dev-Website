aefd3c94857f344d3dd48933b82754c6a0ec0e64
evolvd444 <86843762+evolvd444@users.noreply.github.com>
2023-02-19 23:21:41 -0500

    progress

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import Image from "next/image";
-import { GetServerSideProps } from "next";
+import { GetStaticProps } from "next";
 import Header from "../components/Head";
 import Hero from "../components/Hero";
 import About from "../components/About";
@@ -96,7 +96,7 @@ const Home = ({pageInfo, experiences, skills, projects, socials}: Props) => {
 export default Home;
 
 
-export const getServerSideProps: GetServerSideProps<Props> = async () => {
+export const getStaticProps: GetStaticProps<Props> = async () => {
   const pageInfo: PageInfo = await fetchPageInfo();
   const experiences: Experience[] = await fetchExperiences();
   const skills: Skill[] = await fetchSkills();
@@ -110,13 +110,13 @@ export const getServerSideProps: GetServerSideProps<Props> = async () => {
       skills,
       projects,
       socials,
-      fallback: false,
+      fallback: true,
     },
 
     //Next.js will attempt to regenerate the page
     // - When a request comes in
     // - At most once every 10 seconds
-  
+   revalidate:10,
   };
 
   
